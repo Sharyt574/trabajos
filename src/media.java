a0ee638c5504a4b91f1edce517313bef84cc8f9b
Shary <sharytorocoromajimenezarenas>
2025-02-28 13:37:25 -0500

    se crea la arquitectura java

---
+++ src/media.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class media {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ingrese un numero");
+        int num1 = scanner.nextInt();
+        System.out.println("ingrese un numero");
+        int num2 = scanner.nextInt();
+        System.out.println("ingrese un numero");
+        int num3 = scanner.nextInt();
+        int sum = (num1 + num2 + num3);
+        float promedio = sum / 3 ;
+
+        System.out.println("la media es : " + promedio);
+
+    }
+
+}
